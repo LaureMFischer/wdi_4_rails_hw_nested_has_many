4c91dcace44f8883e54ff3b705d9937eacbf8ad9
Laure Fischer <laurefischer27@gmail.com>
2014-01-29 18:23:30 -0500

    user new view displays form to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(users_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:name, :email)
   end
 end
