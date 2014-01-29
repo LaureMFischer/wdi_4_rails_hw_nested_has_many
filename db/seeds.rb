de3d01026060d19f89616c574f9a5f51ed4300f1
Laure Fischer <laurefischer27@gmail.com>
2014-01-29 18:17:37 -0500

    seeded users table, users index view displays all users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+User.create(name: 'Laure', email: 'laure@gmail.com')
+User.create(name: 'Joe', email: 'joe@gmail.com')
+User.create(name: 'Bill', email: 'bill@gmail.com')
+User.create(name: 'Allison', email: 'allison@gmail.com')
+User.create(name: 'Angela', email: 'angela@gmail.com')
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
