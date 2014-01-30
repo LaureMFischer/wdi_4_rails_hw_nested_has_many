51fb14b8194f7d95c1e8de96ae6c064fe3af25fe
Laure Fischer <laurefischer27@gmail.com>
2014-01-29 20:42:04 -0500

    user show page displays user name, email and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
