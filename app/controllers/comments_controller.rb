56b96becc735c7c585942e0d1d95863f6ff2b3d2
Laure Fischer <laurefischer27@gmail.com>
2014-01-29 23:45:41 -0500

    comments can be added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,20 @@
 class CommentsController < ApplicationController
-  before_action :get_article
+  before_action :get_article, :get_user
   def index
     if params[:article_id]
       @comments = Article.find(:article_id).comments.order(:created_at)
     else
       @comments = Comment.order(:created_at)
     end
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @article.comments << Comment.create!(comments_params)
+    redirect_to user_article_path(@user.id, @article.id)
   end
 
   private
@@ -16,6 +25,10 @@ class CommentsController < ApplicationController
 
   def get_article
     @article = Article.find(params[:article_id]) if params.key?(:article_id)
+  end
+
+  def get_user
+    @user = User.find(params[:user_id]) if params.key?(:user_id)
   end
 
 end
