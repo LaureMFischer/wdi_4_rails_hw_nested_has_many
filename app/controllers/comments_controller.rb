55aca6c8504ff719acc23f5627bb2bb069bc1907
Laure Fischer <laurefischer27@gmail.com>
2014-01-29 23:05:34 -0500

    article show page now includes comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :get_article
+  def index
+    if params[:article_id]
+      @comments = Article.find(:article_id).comments.order(:created_at)
+    else
+      @comments = Comment.order(:created_at)
+    end
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:body, :article_id)
+  end
+
+  def get_article
+    @article = Article.find(params[:article_id]) if params.key?(:article_id)
+  end
+
 end
