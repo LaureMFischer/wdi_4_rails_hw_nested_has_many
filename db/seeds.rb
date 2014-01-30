a41066ea752d79a371ea756de658d41adb7b179a
Laure Fischer <laurefischer27@gmail.com>
2014-01-29 20:31:32 -0500

    seeded database with users, articles and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,17 @@ User.create(name: 'Bill', email: 'bill@gmail.com')
 User.create(name: 'Allison', email: 'allison@gmail.com')
 User.create(name: 'Angela', email: 'angela@gmail.com')
 
+Article.create(body: 'This is an article...it is cool.', title: 'Cool Article', user_id: 1)
+Article.create(body: 'This is another boring article.', title: 'Boring Article', user_id: 2)
+Article.create(body: 'This is a fun article.', title: 'Fun Article', user_id: 3)
+Article.create(body: 'Here is my article.', title: 'My Article', user_id: 4)
+Article.create(body: 'This is one more article with nothing really in it.', title: 'Empty Article', user_id: 5)
 
+Comment.create(body: 'I agree, this was cool.', article_id: 1)
+Comment.create(body: 'I agree, this was boring.', article_id: 2)
+Comment.create(body: 'I agree, this was fun.', article_id: 3)
+Comment.create(body: 'I did not care for this article but respect the effort.', article_id: 4)
+Comment.create(body: 'I don\'t understand why you would publish an empty article.', article_id: 5)
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
