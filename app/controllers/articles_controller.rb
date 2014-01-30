44f544c99bcc0217a7b1dcefee282c7a43601871
Laure Fischer <laurefischer27@gmail.com>
2014-01-29 22:14:55 -0500

    added new article link to user show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,38 @@
 class ArticlesController < ApplicationController
 
+  before_action :get_user
+
   def index
-    @articles = Article.all
+    @articles = Article.order(:created_at)
 
     if params[:user_id]
-      @articles = User.find(:user_id).articles
+      @articles = User.find(:user_id).articles.order(:created_at)
     else
-      @articles = Article.all
+      @articles = Article.order(:created_at)
     end
+
   end
 
   def show
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @user.articles << Article.create!(articles_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :body, :user_id)
+  end
+
+  def get_user
+    @user = User.find(params[:user_id]) if params.key?(:user_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user_id = params[:id]
+    @user = User.find(@user_id)
     @articles = @user.articles
   end
 
